029897c947be44f3c7611dde09fe9d48f3cc2050
WitcherTraining <witcher236@gmail.com>
2020-11-10 16:32:07 +0600

    Enhanced Dataloader, main news page and controller are resolved

--- src/main/java/com/epam/spring/bootstrap/DataLoader.java
+++ src/main/java/com/epam/spring/bootstrap/DataLoader.java
@@ -1,5 +1,6 @@
 package com.epam.spring.bootstrap;
 
+import com.epam.spring.model.Category;
 import com.epam.spring.model.News;
 import com.epam.spring.model.Role;
 import com.epam.spring.model.User;
@@ -36,19 +37,16 @@ public class DataLoader implements ApplicationListener<ContextRefreshedEvent> {
 
         User lera = new User();
 
-        lera.setId(1L);
         lera.setFirstName("Lera");
         lera.setLastName("Kovaleva");
         lera.setLogin("login");
         lera.setPassword("password");
         lera.setRole(Role.ADMIN);
-        lera.setNews(new HashSet<News>());
 
         userService.save(lera);
 
         User roma = new User();
 
-        roma.setId(2L);
         roma.setFirstName("Roma");
         roma.setLastName("Onishenko");
         roma.setLogin("login");
@@ -60,16 +58,47 @@ public class DataLoader implements ApplicationListener<ContextRefreshedEvent> {
 
         System.out.println("Complete to initialize users...");
 
-        News news1 = new News();
+        Category science = new Category();
+        science.setName("Science");
 
-        news1.setId(1L);
-        news1.setTitle("Incredible news");
-        news1.setContent("Huge text Huge textHuge textHuge textHuge textHuge textHuge textHuge textHuge " +
-                "textHuge textHuge textHuge textHuge textHuge textHuge textHuge textHuge textHuge text");
-        news1.setBriefContent("brief text brief text brief text brief text brief text brief text ");
+        categoryService.save(science);
+
+        Category space = new Category();
+        space.setName("Space");
+
+        categoryService.save(space);
+
+        News news1 = new News();
+        news1.setTitle("Pfizer COVID-19 vaccine is 90% effective, preliminary trial data show");
+        news1.setContent("The race to greenlight a COVID-19 vaccine in the United States has entered its final sprint, with one leading candidate becoming the first to release preliminary results showing its vaccine is more than 90 percent effective at preventing people from getting sick from the coronavirus.\n" +
+                "\n" +
+                "The long-awaited announcement came in a Nov. 9 news release detailing the results from an interim analysis of an ongoing Phase III clinical trial comparing the vaccine developed by global pharmaceutical company Pfizer and German biotech company BioNTech with a placebo.\n" +
+                "\n" +
+                "COVID-19 cases are soaring globally, and some countries are reverting to lockdowns and other drastic measures to curb the virus’ spread. As of November 9, more than 50 million people have been infected worldwide — including more than 10 million in the United States — and more than 1.2 million people have died from the disease.");
+        news1.setBriefContent("The vaccine protects people from getting COVID-19, according to an analysis of 94 cases" );
         news1.setAuthor(roma);
+        news1.getCategories().add(science);
+        news1.getCategories().add(space);
 
         newsService.save(news1);
+
+        userService.save(roma);
+
+        News spaceNews = new News();
+        spaceNews.getCategories().add(space);
+        spaceNews.setTitle("About half of Sun-like stars could host rocky, potentially habitable planets");
+        spaceNews.setBriefContent("Since astronomers confirmed the presence of planets beyond our solar system, " +
+                "called exoplanets, humanity has wondered how many could harbor life.");
+        spaceNews.setContent("Our galaxy holds at least an estimated 300 million of these potentially habitable worlds, based on even the most conservative interpretation of the results in a new study to be published in The Astronomical Journal. Some of these exoplanets could even be our interstellar neighbors, with at least four potentially within 30 light-years of our Sun and the closest likely to be at most about 20 light-years from us. These are the minimum numbers of such planets based on the most conservative estimate that 7% of Sun-like stars host such worlds. However, at the average expected rate of 50%, there could be many more.\n" +
+                "\n" +
+                "This research helps us understand the potential for these planets to have the elements to support life. This is an essential part of astrobiology, the study of life's origins and future in our universe.\n" +
+                "\n" +
+                "The study is authored by NASA scientists who worked on the Kepler mission alongside collaborators from around the world. NASA retired the space telescope in 2018 after it ran out of fuel. Nine years of the telescope's observations revealed that there are billions of planets in our galaxy -- more planets than stars.");
+        spaceNews.setAuthor(roma);
+
+        newsService.save(spaceNews);
+
+        userService.save(roma);
 
         System.out.println("Complete to initialize news...");
     }
--- src/main/java/com/epam/spring/controllers/IndexController.java
+++ src/main/java/com/epam/spring/controllers/IndexController.java
@@ -1,13 +1,24 @@
 package com.epam.spring.controllers;
 
+import com.epam.spring.services.NewsService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
+    private final NewsService newsService;
+
+    public IndexController(NewsService newsService) {
+        this.newsService = newsService;
+    }
+
     @GetMapping({"", "/", "index"})
-    public String getIndexPage(){
+    public String getIndexPage(Model model){
+
+        model.addAttribute("news", newsService.findAll());
+
         return "index";
     }
 }

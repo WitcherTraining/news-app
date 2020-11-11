cce55ea86e2f26ef1665995b7ee487db9af5d2a7
WitcherTraining <witcher236@gmail.com>
2020-11-11 12:33:37 +0600

    Added Spring profiles config for data sources

---
+++ src/main/java/com/epam/spring/config/SpringProfilesConfig.java
@@ -0,0 +1,15 @@
+package com.epam.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.WebApplicationInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+@Configuration
+public class SpringProfilesConfig implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        servletContext.setInitParameter("spring.profiles.active", "map");
+    }
+}

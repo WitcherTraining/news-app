26bf7a8e0e032adaf263216967a78628e02d9d44
WitcherTraining <witcher236@gmail.com>
2020-11-14 23:08:04 +0600

    Added update, delete and create options for News

--- src/main/java/com/epam/spring/controllers/NewsController.java
+++ src/main/java/com/epam/spring/controllers/NewsController.java
@@ -1,13 +1,17 @@
 package com.epam.spring.controllers;
 
 import com.epam.spring.model.News;
+import com.epam.spring.model.User;
 import com.epam.spring.services.NewsService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
+@Slf4j
 @RequestMapping("/news")
 @Controller
 public class NewsController {
@@ -20,12 +24,12 @@ public class NewsController {
 
     @GetMapping("/{id}/show")
     public String showById(@PathVariable String id, Model model){
-        model.addAttribute("oneNews", newsService.findById(Long.valueOf(id)));
+        model.addAttribute("news", newsService.findById(Long.valueOf(id)));
         return "news/show";
     }
 
     @GetMapping("/{newsId}/edit")
-    public String initUpdateNewsForm(@PathVariable String newsId, Model model){
+    public String initUpdateForm(@PathVariable String newsId, Model model){
         model.addAttribute(newsService.findById(Long.valueOf(newsId)));
         return "news/createOrUpdateNewsForm";
     }
@@ -36,5 +40,36 @@ public class NewsController {
         return "news/createOrUpdateNewsForm";
     }
 
+    @PostMapping("{newsId}/show")
+    public String processUpdateForm(@Valid News news, User user, Model model, BindingResult result){
+        if (result.hasErrors()) {
+            news.setAuthor(user);
+            model.addAttribute("news", news);
+            return "news/createOrUpdateNewsForm";
+        } else {
+            user.getNews().add(news);
+            News savedNews = newsService.save(news);
+            return "redirect:/news/" + savedNews.getId() + "/show";
+        }
+    }
 
+    @PostMapping("/new")
+    public String processCreationForm(@Valid News news, BindingResult result) {
+        if (result.hasErrors()) {
+            return "news/createOrUpdateNewsForm";
+        } else {
+            News savedNews = newsService.save(news);
+            return "redirect:/news/" + savedNews.getId() + "/show";
+        }
+    }
+
+    @GetMapping("{id}/delete")
+    public String deleteById(@PathVariable String id){
+
+        log.debug("Deleting news with id: " + id);
+
+        newsService.deleteById(Long.valueOf(id));
+
+        return "redirect:/index";
+    }
 }

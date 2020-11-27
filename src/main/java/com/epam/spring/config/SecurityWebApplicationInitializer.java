3198843ac2dedc6edbfc8732263e520d5ba090ac
WitcherTraining <witcher236@gmail.com>
2020-11-27 11:40:16 +0600

    Added Security configuration classes and dependencies

---
+++ src/main/java/com/epam/spring/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package com.epam.spring.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
